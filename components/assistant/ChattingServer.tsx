d58a09d00c4a542eb42fdb1439c1268a1988442b
Ahmad Abuwardeh <ahm.abuwardeh@gmail.com>
2024-07-15 18:19:59 +0300

    fix type error

--- components/assistant/ChattingServer.tsx
+++ components/assistant/ChattingServer.tsx
@@ -3,17 +3,19 @@ import { cookies } from 'next/headers';
 import React from 'react';
 import dynamic from 'next/dynamic';
 import { supabase } from "@/lib/supabaseClient";
+import { HistoryItem, Thread } from "@/types";
 
 const prisma = new PrismaClient();
 const Chatting = dynamic(() => import('@/components/assistant/Chatting'), { ssr: false });
 
-async function fetchThreads(userId: string) {
-    const threads = await prisma.thread.findMany({
+
+async function fetchThreads(userId: string): Promise<HistoryItem[]> {
+    const threads: Thread[] = await prisma.thread.findMany({
         where: { userId },
         orderBy: { createdAt: 'desc' },
     });
 
-    return threads.map(thread => ({
+    return threads.map((thread: Thread) => ({
         id: thread.id,
         title: thread.content,
         details: new Date(thread.createdAt).toLocaleString(),
